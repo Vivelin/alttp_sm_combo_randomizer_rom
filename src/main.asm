c989d6cf5a8d705ef45ac91ed522cabf81e29879
Laura Verdoes <1766841+Vivelin@users.noreply.github.com>
2022-07-11 16:57:36 +0200

    Added Easier Wall Jump patch
    Patch by Benox50 from https://metroidconstruction.com/resource.php?id=545

--- src/main.asm
+++ src/main.asm
@@ -35,6 +35,7 @@ incsrc "sm/titlescreen.asm"                 ; Super Metroid Upload New Title Scr
 incsrc "sm/keycards.asm"                    ; Super Metroid Keycard system
 incsrc "sm/plminject.asm"                   ; Super Metroid PLM Injection
 incsrc "sm/messagebox.asm"                  ; Super Metroid Messagebox improvements
+incsrc "sm/EasierWJ.asm"                    ; Super Metroid Easier Wall Jump by Benox50
 
 ; --- ALTTP code ---
 incsrc "z3/hirom.asm"	                    ; ALTTP ExHiROM patch
